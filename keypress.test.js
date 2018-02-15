cc90021fb39d8091e2125dd86f9fecc39ff7f792
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-02-15 15:22:32 +0000

    halfway house, need to test the actual numerical result

--- keypress.test.js
+++ keypress.test.js
@@ -10,9 +10,32 @@ function presses(phrase) {
     ['T', 'U', 'V'],
     ['W', 'X', 'Y', 'Z']
   ];
+
+  findValueInArray = (letter, array) => {
+    // var letter = letter;
+    // var result = '';
+    // var moo = '';
+    array.filter(function(arrayItem) {
+      result = arrayItem.find(function(arrayletter) {
+        // console.log(arrayletter); // A B C ...
+        // console.log(letter); // L O L
+        // console.log(arrayItem); // ['A', 'B', 'C'] ...
+
+        // console.log(letter === arrayletter ? arrayItem.indexOf(letter) + 1 : false);
+        var ooo = letter === arrayletter ? arrayItem.indexOf(letter) + 1 : false;
+        if (ooo) {
+          moo = ooo;
+        }
+      });
+    });
+    // console.log(moo);
+    return moo;
+  }
+
   var message = phrase.split('');
   message.forEach(function(letter){
-    console.log(letter);
+    console.log(findValueInArray(letter, keyArray));
+    findValueInArray(letter, keyArray);
   });
   return message;
 }
@@ -20,4 +43,9 @@ function presses(phrase) {
 it('should split the phrase into characters and add them to an array', () => {
   const pressesFunc = presses('LOL');
   expect(pressesFunc).toEqual(['L', 'O', 'L']);
+});
+
+it('should return the position of each letter in their array added together', () => {
+  const pressesFunc = presses('LOL');
+  expect(pressesFunc).toEqual(9);
 });
