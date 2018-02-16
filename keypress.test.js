49c841e011e47b4c2bb443448e2a11e429cbe695
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-02-16 15:03:31 +0000

    enabled linting, tidy up

--- keypress.test.js
+++ keypress.test.js
@@ -18,7 +18,7 @@ function presses(phrase) {
   const message = phrase.toUpperCase().split('');
   const resultArray = [];
 
-  findValueInArray = (letterInMessage, array) => {
+  function findValueInArray(letterInMessage, array) {
     let amountOfKeyPresses;
     letterInMessage === ' ' ? 1 : letterInMessage;
 
@@ -40,52 +40,52 @@ function presses(phrase) {
   return resultArray.reduce((accumulator, currentValue) => accumulator + currentValue);
 }
 
-it('should allow input in lower case', () => {
+it('should allow input in lower case', () => { // eslint-disable-line no-undef
   const pressesFunc = presses('hello');
-  expect(pressesFunc).toEqual(13);
+  expect(pressesFunc).toEqual(13); // eslint-disable-line no-undef
 });
 
-it('should allow input in lower case with spaces', () => {
+it('should allow input in lower case with spaces', () => { // eslint-disable-line no-undef
   const pressesFunc = presses('c u l8r');
-  expect(pressesFunc).toEqual(17);
+  expect(pressesFunc).toEqual(17); // eslint-disable-line no-undef
 });
 
-it('should return 9 for the word \'LOL\'', () => {
+it('should return 9 for the word \'LOL\'', () => { // eslint-disable-line no-undef
   const pressesFunc = presses('LOL');
-  expect(pressesFunc).toEqual(9);
+  expect(pressesFunc).toEqual(9); // eslint-disable-line no-undef
 });
 
-it('should return 7 for the name \'PAUL\'', () => {
+it('should return 7 for the name \'PAUL\'', () => { // eslint-disable-line no-undef
   const pressesFunc = presses('PAUL');
-  expect(pressesFunc).toEqual(7);
+  expect(pressesFunc).toEqual(7); // eslint-disable-line no-undef
 });
 
-it('should return 22 for the name \'BROWNSMITH\'', () => {
+it('should return 22 for the name \'BROWNSMITH\'', () => { // eslint-disable-line no-undef
   const pressesFunc = presses('BROWNSMITH');
-  expect(pressesFunc).toEqual(22);
+  expect(pressesFunc).toEqual(22); // eslint-disable-line no-undef
 });
 
-it('should handle spaces by returning 30 for the name \'PAUL BROWNSMITH\'', () => {
+it('should handle spaces by returning 30 for the name \'PAUL BROWNSMITH\'', () => { // eslint-disable-line no-undef
   const pressesFunc = presses('PAUL BROWNSMITH');
-  expect(pressesFunc).toEqual(30);
+  expect(pressesFunc).toEqual(30); // eslint-disable-line no-undef
 });
 
-it('should handle spaces by returning 23 for the phrase \'AS YOU WERE\'', () => {
+it('should handle spaces by returning 23 for the phrase \'AS YOU WERE\'', () => { // eslint-disable-line no-undef
   const pressesFunc = presses('AS YOU WERE');
-  expect(pressesFunc).toEqual(23);
+  expect(pressesFunc).toEqual(23); // eslint-disable-line no-undef
 });
 
-it('should handle numbers by returning 17 for the phrase \'C U L8R\'', () => {
+it('should handle numbers by returning 17 for the phrase \'C U L8R\'', () => { // eslint-disable-line no-undef
   const pressesFunc = presses('C U L8R');
-  expect(pressesFunc).toEqual(17);
+  expect(pressesFunc).toEqual(17); // eslint-disable-line no-undef
 });
 
-it('should handle numbers by returning 11 for the phrase \'C3PO\'', () => {
+it('should handle numbers by returning 11 for the phrase \'C3PO\'', () => { // eslint-disable-line no-undef
   const pressesFunc = presses('C3PO');
-  expect(pressesFunc).toEqual(11);
+  expect(pressesFunc).toEqual(11); // eslint-disable-line no-undef
 });
 
-it('should handle numbers by returning 12 for the phrase \'R2D2\'', () => {
+it('should handle numbers by returning 12 for the phrase \'R2D2\'', () => { // eslint-disable-line no-undef
   const pressesFunc = presses('R2D2');
-  expect(pressesFunc).toEqual(12);
+  expect(pressesFunc).toEqual(12); // eslint-disable-line no-undef
 });
